617479d3fa245a86ea48f7718a912809638a6642
polina.kotelnikova <p.kotelnikova@i-teco.ru>
2021-05-25 12:25:02 +0300

    init

---
+++ src/main/kotlin/com/example/demo444/Demo444Application.kt
@@ -0,0 +1,11 @@
+package com.example.demo444
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Demo444Application
+
+fun main(args: Array<String>) {
+    runApplication<Demo444Application>(*args)
+}
